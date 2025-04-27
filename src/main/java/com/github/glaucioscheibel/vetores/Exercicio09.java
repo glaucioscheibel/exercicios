540185e3fcd0ae431fb72d698b5af7668dea6834
Glaucio Scheibel <gscheibel@gmail.com>
2025-04-27 17:17:27 -0300

    Algumas correções

--- src/main/java/com/github/glaucioscheibel/vetores/Exercicio09.java
+++ src/main/java/com/github/glaucioscheibel/vetores/Exercicio09.java
@@ -1,32 +1,35 @@
 package com.github.glaucioscheibel.vetores;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 
 public class Exercicio09 {
     public static void main(String[] args) {
-        int[] A = {1, 2, 3, 4, 5, 20, 30, 50};
-        int[] B = {6, 7, 8, 9, 10, 25, 35, 55, 65, 200};
+        int[] a = {1, 2, 3, 4, 5, 20, 30, 50};
+        int[] b = {6, 7, 8, 9, 10, 25, 35, 55, 65, 200};
+        int[] c = new int[a.length + b.length];
         int ai = 0;
         int bi = 0;
-        List<Integer> C = new ArrayList<>();
-        while (ai < A.length && bi < B.length) {
-            if (A[ai] < B[bi]) {
-                C.add(A[ai]);
+        int ci = 0;
+        while (ai < a.length && bi < b.length) {
+            if (a[ai] < b[bi]) {
+                c[ci] = a[ai];
                 ai++;
             } else {
-                C.add(B[bi]);
+                c[ci] = b[bi];
                 bi++;
             }
+            ci++;
         }
-        while (ai < A.length) {
-            C.add(A[ai]);
+        while (ai < a.length) {
+            c[ci] = a[ai];
             ai++;
+            ci++;
         }
-        while (bi < B.length) {
-            C.add(B[bi]);
+        while (bi < b.length) {
+            c[ci] = b[bi];
             bi++;
+            ci++;
         }
-        System.out.printf("Mesclado: %s\n", C);
+        System.out.printf("Mesclado: %s\n", Arrays.toString(c));
     }
 }
