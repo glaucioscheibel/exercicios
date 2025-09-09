9b59e3227cb08bb6e0e8b19434ca5ddf4c2fa455
Glaucio Scheibel <gscheibel@gmail.com>
2025-09-09 20:16:36 -0300

    Bug fix

--- src/main/java/com/github/glaucioscheibel/concorrente/exercicio05/Exercicio05.java
+++ src/main/java/com/github/glaucioscheibel/concorrente/exercicio05/Exercicio05.java
@@ -1,6 +1,8 @@
 package com.github.glaucioscheibel.concorrente.exercicio05;
 
 public class Exercicio05 {
+    private static final int _3_MINUTOS = 3 * 60 * 1000;
+
     public static void main(String[] args) throws Exception {
         Banco firmeza = new Banco(100, 1000D);
         System.out.printf("Saldo do banco: %,.2f Transações %,d\n", firmeza.getSaldoTotal(), firmeza.getTransacoes());
@@ -9,10 +11,14 @@ public class Exercicio05 {
         Thread.ofVirtual().start(new Movimento(firmeza));
         Thread.ofVirtual().start(new Movimento(firmeza));
         Thread.ofVirtual().start(new Movimento(firmeza));
+        long ini = System.currentTimeMillis();
         while (true) {
             Thread.sleep(5000);
             System.out.printf(
                     "Saldo do banco: %,.2f Transações %,d\n", firmeza.getSaldoTotal(), firmeza.getTransacoes());
+            if (System.currentTimeMillis() - ini >= _3_MINUTOS) {
+                break;
+            }
         }
     }
 }
