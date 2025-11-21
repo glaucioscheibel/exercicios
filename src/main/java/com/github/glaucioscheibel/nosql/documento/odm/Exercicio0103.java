c59c58bca11e38e5f2df23b3405a4a989b284e47
Glaucio Scheibel <gscheibel@gmail.com>
2025-11-21 19:57:14 -0300

    Minor fix

--- src/main/java/com/github/glaucioscheibel/nosql/documento/odm/Exercicio0103.java
+++ src/main/java/com/github/glaucioscheibel/nosql/documento/odm/Exercicio0103.java
@@ -1,17 +1,18 @@
 package com.github.glaucioscheibel.nosql.documento.odm;
 
+import static dev.morphia.query.filters.Filters.eq;
+
 import com.github.glaucioscheibel.nosql.documento.odm.model.Autor;
 import com.mongodb.client.MongoClients;
 import dev.morphia.Datastore;
 import dev.morphia.Morphia;
 import dev.morphia.query.Query;
-import dev.morphia.query.filters.Filters;
 
 public class Exercicio0103 {
     public static void main(String[] args) {
         Datastore datastore = Morphia.createDatastore(MongoClients.create());
         Query<Autor> query = datastore.find(Autor.class);
-        query = query.filter(Filters.eq("nome", "Edgar Allan Poe"));
+        query = query.filter(eq("nome", "Edgar Allan Poe"));
         Autor edgar = query.first();
         System.out.println(edgar);
     }
