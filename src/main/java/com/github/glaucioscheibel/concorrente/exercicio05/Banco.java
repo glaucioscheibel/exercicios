ffe335ea77c306dd5235bb73b152fa02feb0ada8
Glaucio Scheibel <gscheibel@gmail.com>
2026-03-25 22:04:08 -0300

    Melhora de performance no exercício do banco

--- src/main/java/com/github/glaucioscheibel/concorrente/exercicio05/Banco.java
+++ src/main/java/com/github/glaucioscheibel/concorrente/exercicio05/Banco.java
@@ -5,27 +5,42 @@ import java.util.List;
 
 public class Banco {
     private List<Conta> contas;
-    private int transacoes;
+    private volatile int transacoes;
+    private Object monitor;
 
     public Banco(int qtdeContas, double saldoInicial) {
         contas = new ArrayList<>();
         for (int i = 0; i < qtdeContas; i++) {
             contas.add(new Conta(saldoInicial));
         }
+        monitor = new Object();
     }
 
-    public synchronized void transferencia(int contaDebito, int contaCredito, double valor) {
-        contas.get(contaDebito).saque(valor);
-        contas.get(contaCredito).deposito(valor);
-        transacoes++;
-    }
-
-    public synchronized double getSaldoTotal() {
-        double saldoTotal = 0D;
-        for (Conta conta : contas) {
-            saldoTotal += conta.getSaldo();
+    public void transferencia(int contaDebito, int contaCredito, double valor) {
+        if (contaDebito == contaCredito) {
+            return;
         }
-        return saldoTotal;
+        synchronized (monitor) {
+            int menor = Math.min(contaDebito, contaCredito);
+            int maior = Math.max(contaDebito, contaCredito);
+            synchronized (contas.get(menor)) {
+                synchronized (contas.get(maior)) {
+                    contas.get(contaDebito).saque(valor);
+                    contas.get(contaCredito).deposito(valor);
+                }
+            }
+            transacoes++;
+        }
+    }
+
+    public double getSaldoTotal() {
+        synchronized (monitor) {
+            double saldoTotal = 0D;
+            for (Conta conta : contas) {
+                saldoTotal += conta.getSaldo();
+            }
+            return saldoTotal;
+        }
     }
 
     public int getTransacoes() {
