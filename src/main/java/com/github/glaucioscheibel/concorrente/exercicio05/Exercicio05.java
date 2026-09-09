b7b32ef7b72debdc5d7bcd2ce2bb81a630edd069
Glaucio Scheibel <gscheibel@gmail.com>
2026-09-09 15:30:22 -0300

    Minor fix

--- src/main/java/com/github/glaucioscheibel/concorrente/exercicio05/Exercicio05.java
+++ src/main/java/com/github/glaucioscheibel/concorrente/exercicio05/Exercicio05.java
@@ -1,7 +1,7 @@
 package com.github.glaucioscheibel.concorrente.exercicio05;
 
 public class Exercicio05 {
-    private static final int TRES_MINUTOS = 1 * 60 * 1000;
+    private static final int TRES_MINUTOS = 3 * 60 * 1000;
 
     static void main() throws Exception {
         Banco firmeza = new Banco(100, 1000D);
